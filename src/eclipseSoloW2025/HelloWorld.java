d971c70a3d527fd850abb208607a5de45a217c25
tsavrami <tsavrami@emich.edu>
2025-02-03 12:14:13 -0500

    For step 6.2

--- src/eclipseSoloW2025/HelloWorld.java
+++ src/eclipseSoloW2025/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Added this println statement for step 4, and trying again...");
-		}
+		System.out.println("Programmer 2 did this"); // Part D, step 6.2
+	}
 
 }
 
