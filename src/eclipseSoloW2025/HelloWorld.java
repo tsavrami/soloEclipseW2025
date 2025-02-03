e7f43fd6b908a1a947e570f3fc0256ee898499d3
tsavrami <tsavrami@emich.edu>
2025-02-03 11:27:22 -0500

    Initial commit

---
+++ src/eclipseSoloW2025/HelloWorld.java
@@ -0,0 +1,12 @@
+package eclipseSoloW2025;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+// https://github.com/tsavrami/soloEclipseW2025.git
