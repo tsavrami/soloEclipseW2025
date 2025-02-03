da27992175ec7fd06a190b538351f0efab3ef1a3
tsavrami <tsavrami@emich.edu>
2025-02-03 13:00:41 -0500

    Unfixing grammar idk

--- src/eclipseSoloW2025/HelloWorld.java
+++ src/eclipseSoloW2025/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Added this println statement for step 4, and trying again...");
-		System.out.println("Programmer2 did this Programmer1 did this");
+		System.out.println("Programmer 2 did this Programmer1 did this");
 		}
 
 }
